6e1b59e94c7dd5e841830181367669a1f092b506
John Doty <john@d0ty.me>
2017-07-28 07:06:33 -0700

    Cleanup caches and upgrade node version for build speed

--- onceandfuture/build-image.sh
+++ onceandfuture/build-image.sh
@@ -3,22 +3,36 @@
 # Clean up as hard as you can at the end, because everything still on disk
 # when we're finished adds to the size of the image.
 #
-# I know, set -e is not recommended, but it works for this particular script and makes it so much more readable.
-# Proper tools for the proper job.
+# I know, set -e is not recommended, but it works for this particular script
+# and makes it so much more readable.  Proper tools for the proper job.
 set -e
 
+echo ======================================
+echo INSTALLING SECURITY PATCHES
+echo ======================================
 apt-get update -q
 apt-get dist-upgrade -y -q
+
+echo ======================================
+echo INSTALLING REQUIRED PACKAGES
+echo ======================================
 apt-get install -y -q curl xz-utils
 
+echo ======================================
+echo BUILDING APP
+echo ======================================
 dotnet restore
 dotnet build --configuration="Release"
 
+echo ======================================
+echo BUILDING PRODUCTION JS
+echo ======================================
 # The checked-in bundle is debug; generate the production JS.
-NODE_VERSION=node-v6.9.2-linux-x64
+NODE_VERSION_NUMBER=v8.2.1
+NODE_VERSION=node-$NODE_VERSION_NUMBER-linux-x64
 mkdir /tmp/nodejs
 cd /tmp/nodejs
-curl -SL https://nodejs.org/dist/v6.9.2/$NODE_VERSION.tar.xz -o $NODE_VERSION.tar.xz
+curl -SL https://nodejs.org/dist/$NODE_VERSION_NUMBER/$NODE_VERSION.tar.xz -o $NODE_VERSION.tar.xz
 tar xf $NODE_VERSION.tar.xz
 cd /app
 NODE_BIN_PATH=/tmp/nodejs/$NODE_VERSION/bin
@@ -31,6 +45,16 @@ rm -rf /tmp/nodejs
 # CLEANUP
 cd /app
 
+echo ======================================
+echo REMOVING PACKAGES
+echo ======================================
 apt-get remove -q -y curl xz-utils
 apt-get autoremove -y
+
+echo ======================================
+echo REMOVING CACHE THINGS
+echo ======================================
 rm -rf /var/lib/apt/lists/*
+rm -rf /root/.nuget
+rm -rf /root/.npm
+rm -rf /root/.local
